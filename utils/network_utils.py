8cab620bdfe915b053348e28ede329c5d86b3bc8
pswapnesh <swapnesh.off@gmail.com>
2025-02-06 10:47:10 +0100

    make net

--- utils/network_utils.py
+++ utils/network_utils.py
@@ -1,19 +1,41 @@
 import networkx as nx
 import numpy as np
 
-def make_network(hdb,df, wt_nan = 1e9):
+
+def make_network(hdb, df, wt_nan=1e12,min_lambda = -1):
+    """
+    Constructs a directed graph from HDBSCAN's condensed tree.
+
+    Parameters:
+        hdb: HDBSCAN object with condensed_tree_ attribute.
+        df (pd.DataFrame): Dataframe containing node attributes.
+        wt_nan (float): Weight assigned when lambda_val is NaN.
+
+    Returns:
+        nx.DiGraph: A directed graph with nodes and edges from the condensed tree.
+    """
     G = nx.DiGraph()
-    for row in hdb._condensed_tree:
-        if np.isfinite(row['lambda_val']):
-            w = row['lambda_val']
+
+    # Add all nodes from the condensed tree
+    all_nodes = set(hdb.condensed_tree_._raw_tree['parent']).union(set(hdb.condensed_tree_._raw_tree['child']))
+    G.add_nodes_from(all_nodes)
+
+    # Add edges with weights
+    for row in hdb.condensed_tree_._raw_tree:
+        parent, child, lambda_val = int(row['parent']), int(row['child']), row['lambda_val']
+        weight = lambda_val if np.isfinite(lambda_val) else wt_nan
+        if min_lambda > 0:
+            if lambda_val > min_lambda:                
+                G.add_edge(parent, child, weight=weight, distance=1 / weight if weight != 0 else wt_nan)
         else:
-            w = wt_nan
-        G.add_edge(int(row['parent']), int(row['child']),weight = w)
+            G.add_edge(parent, child, weight=weight, distance=1 / weight if weight != 0 else wt_nan)
 
     # Assign attributes to existing nodes
-    for i,(idx, row) in enumerate(df.iterrows()):
-        nodeId = int(i)
-        G.nodes(data=True)[nodeId].update(row.to_dict())
+    for i, (idx, row) in enumerate(df.iterrows()):
+        node_id = int(i)
+        if node_id in G.nodes:
+            G.nodes[node_id].update(row.to_dict())
+
     return G
 
 # def traverse_graph_and_get_accessions(G, query_accessions, n):
